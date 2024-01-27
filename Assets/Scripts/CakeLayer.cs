dd21b49704a81c61823e1d21f1d3f91bcce6fc2e
TheAsteroid <41196601+TheAsteroid@users.noreply.github.com>
2024-01-27 19:02:13 +0100

    Add soundtrack and layer sound FX

--- Assets/Scripts/CakeLayer.cs
+++ Assets/Scripts/CakeLayer.cs
@@ -1,15 +1,46 @@
-using System.Collections;
-using System.Collections.Generic;
+using System;
 using UnityEngine;
 
 public class CakeLayer : MonoBehaviour
 {
-    [SerializeField] AudioSource _audioSource;
+    private bool _hasLanded = false;
 
-    //Use it if needed
+    [SerializeField] AudioClip[] _fallingClips;
+    [SerializeField] AudioClip[] _landedClips;
+
+    [SerializeField] AudioSource _fallingSource;
+    [SerializeField] AudioSource _landedSource;
+
+    private void Start()
+    {
+        PlayRandomClip(_fallingClips, _fallingSource);
+    }
 
     private void OnCollisionEnter(Collision collision)
     {
-        Debug.Log("Splatter");
+        if (_hasLanded)
+        {
+            return;
+        }
+        _hasLanded = true;
+
+        Debug.Log("Collision started");
+        PlayRandomClip(_landedClips, _landedSource);
+        Debug.Log("Collision ended");
+    }
+
+    private void PlayRandomClip(AudioClip[] audioClips, AudioSource audioSource)
+    {
+        if ( audioClips.Length == 0)
+        {
+            throw new InvalidOperationException("No AudioClips available on layer.");
+        }
+
+        if(audioSource == null)
+        {
+            throw new InvalidOperationException("No AudioSource defined.");
+        }
+
+        audioSource.PlayOneShot(audioClips[UnityEngine.Random.Range(0, audioClips.Length - 1)]);
     }
 }

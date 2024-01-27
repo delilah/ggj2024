9555690209f0964c93134a54dc8a9e434def8722
Delilah <delstuff@gmail.com>
2024-01-28 00:46:13 +0100

    beat script updated + minor adjustments to scene/prefabs

--- Assets/Scripts/Midi/MidiTest.cs
+++ Assets/Scripts/Midi/MidiTest.cs
@@ -7,6 +7,7 @@ public class MidiTest : MonoBehaviour
     public List<TimedNote> notes;
     private Queue<TimedNote> _notesQueue;
     private List<GameObject> _spawnedNotes = new List<GameObject>();
+    private Dictionary<GameObject, float> _speedMultipliers = new Dictionary<GameObject, float>();
 
     public GameObject NoteA;
     public GameObject NoteB;
@@ -22,80 +23,127 @@ public class MidiTest : MonoBehaviour
     [SerializeField] private GameObject _clearPrefabsPoint;
 
     private float _currentTime = 0f;
-    public float speedMultiplier = 1f;
 
     void Start()
     {
         notes = MidiReader.GetNotesList("songtest");
         _notesQueue = new Queue<TimedNote>(notes);
+
+       // foreach (var note in notes)
+       // {
+      //      Debug.Log("Note time: " + note.TimeInSeconds);
+        //}
     }
 
     void Update()
     {
         _currentTime += Time.deltaTime;
-
-        while (_notesQueue.Count > 0 && _notesQueue.Peek().TimeInSeconds <= _currentTime)
-        {
-            TimedNote currentNote = _notesQueue.Dequeue();
-            GameObject noteSpawned = SpawnNotePrefab(currentNote);
-            _spawnedNotes.Add(noteSpawned);
-        }
-
-
-        for (int i = 0; i < _spawnedNotes.Count; i++)
-        {
-            var spawnedNote = _spawnedNotes[i];
-            Vector3 movement = new Vector3(-speedMultiplier * Time.deltaTime, 0, 0);
-            spawnedNote.transform.Translate(movement);
-        }
-
-        for (int i = _spawnedNotes.Count - 1; i >= 0; i--)
-        {
-            var spawnedNote = _spawnedNotes[i];
-            if (spawnedNote.transform.position.x < _clearPrefabsPoint.transform.position.x)
-            {
-                Destroy(spawnedNote);
-                _spawnedNotes.RemoveAt(i);
-            }
-        }
+        SpawnNotes();
+        MoveAndClearNotes();
     }
 
-    GameObject SpawnNotePrefab(TimedNote currentNote)
+void SpawnNotes()
+{
+    const float speed = 8f; // Set a constant speed
+    var spawnDistance = _spawnNotesXPos.transform.position.x;
+    
+    while (_notesQueue.Count > 0)
     {
-        GameObject noteToSpawn = null;
-        float heightAdjustment = 0f;
+        var currentNote = _notesQueue.Peek();
+        var spawnTime = currentNote.TimeInSeconds - spawnDistance / speed;
+        
+        // If it's not yet time to spawn this note, break the loop
+        if (spawnTime > _currentTime) break;
 
-        switch (currentNote.Note)
-        {
-            case 36:
-                noteToSpawn = NoteA;
-                heightAdjustment = _lineA.position.y;
-                break;
-            case 37:
-                noteToSpawn = NoteB;
-                heightAdjustment = _lineB.position.y;
-                break;
-            case 38:
-                noteToSpawn = NoteC;
-                heightAdjustment = _lineC.position.y;
-                break;
-            case 39:
-                noteToSpawn = NoteD;
-                heightAdjustment = _lineD.position.y;
-                break;
-            default:
-                Debug.LogError($"Unexpected note number: {currentNote.Note}");
-                return null;
-        }
+        var spawnedNote = SpawnNotePrefab(currentNote);
+        _notesQueue.Dequeue();
 
-        if (noteToSpawn != null)
+        _speedMultipliers[spawnedNote] = speed;
+        _spawnedNotes.Add(spawnedNote);
+    }
+}
+
+void MoveAndClearNotes()
+{
+    for (int i = _spawnedNotes.Count - 1; i >= 0; i--)
+    {
+        var spawnedNote = _spawnedNotes[i];
+        var speed = _speedMultipliers[spawnedNote];
+        var movement = new Vector3(-speed * Time.deltaTime, 0, 0);
+
+        //DebugZero(spawnedNote, movement);
+
+        // Calculate new position
+        var potentialNewPos = spawnedNote.transform.position + movement;
+
+        // Calculate position in relation to the clearPrefabsPoint and evaluate cleanup
+        if (potentialNewPos.x < _clearPrefabsPoint.transform.position.x)
         {
-            Vector3 adjustedPosition = new Vector3(_spawnNotesXPos.transform.position.x, heightAdjustment, _spawnNotesXPos.transform.position.z);
-            return Instantiate(noteToSpawn, adjustedPosition, Quaternion.identity);
+            _speedMultipliers.Remove(spawnedNote);
+            Destroy(spawnedNote);
+            _spawnedNotes.RemoveAt(i);
         }
         else
         {
-            return null;
+            // Keep moving until reaches the ClearPrefabsPoint
+            spawnedNote.transform.Translate(movement);
+        }
+    }
+}
+
+void DebugZero(GameObject spawnedNote, Vector3 movement)
+{
+    // Debug to check the value of the prefab when it reaches the middle/hot area
+    if (spawnedNote.transform.position.x >= 0 && spawnedNote.transform.position.x + movement.x < 0)
+    {
+        Debug.Log($"Prefab {spawnedNote.name} crosses x=0 at time {_currentTime} ");
+    }
+}
+
+
+    GameObject SpawnNotePrefab(TimedNote currentNote)
+    {
+        var noteToSpawn = GetNoteToSpawn(currentNote.Note);
+        if (noteToSpawn == null) return null;
+        
+        var heightAdjustment = GetHeightAdjustment(currentNote.Note);
+        var adjustedPosition = new Vector3(_spawnNotesXPos.transform.position.x, heightAdjustment, _spawnNotesXPos.transform.position.z);
+        return Instantiate(noteToSpawn, adjustedPosition, Quaternion.identity);
+    }
+
+    GameObject GetNoteToSpawn(int note)
+    {
+        switch (note)
+        {
+            case 36:
+                return NoteA;
+            case 37:
+                return NoteB;
+            case 38:
+                return NoteC;
+            case 39:
+                return NoteD;
+            default:
+                Debug.LogError($"Unexpected note number: {note}");
+                return null;
+        }
+    }
+
+    float GetHeightAdjustment(int note)
+    {
+        switch (note)
+        {
+            case 36:
+                return _lineA.position.y;
+            case 37:
+                return _lineB.position.y;
+            case 38:
+                return _lineC.position.y;
+            case 39:
+                return _lineD.position.y;
+            default:
+                Debug.LogError($"Unexpected note number: {note}");
+                return 0;
         }
     }
 }

81d92c3f1c6562d1650d49867e4832b104932ae0
Delilah <delstuff@gmail.com>
2024-01-28 01:52:46 +0100

    Added enum for the paws + actions

--- Assets/Scripts/Paws/SmashTheNotes.cs
+++ Assets/Scripts/Paws/SmashTheNotes.cs
@@ -1,4 +1,11 @@
+using System;
 using UnityEngine;
+
+public enum PawType
+{
+    Left,
+    Right
+}
 
 public class SmashTheNotes : MonoBehaviour
 {
@@ -7,43 +14,34 @@ public class SmashTheNotes : MonoBehaviour
     private bool hasMoved = false;
     private bool hitDetected = false;
 
-    [SerializeField] private float _offsetLeft; 
-    [SerializeField] private float _offsetRight; 
-
+    [SerializeField] private PawType pawType;
+    [SerializeField] private float _offsetLeft = 1.33f;
+    [SerializeField] private float _offsetRight = 2.0f;
     [SerializeField] private float colliderRadius;
     [SerializeField] private LayerMask noteLayer;
 
-    void Start () 
+    // Actions
+    public static event Action GoodLayer;
+    public static event Action BadLayer;
+
+    void Start()
     {
         initialPosition = this.transform.position;
-        _offsetLeft = 1.33f;
-        _offsetRight = 2.0f;
     }
 
     void Update()
     {
-
-          if (gameObject.name == "PawLeft")
-        {
-            if (Input.GetKey(KeyCode.LeftArrow) || Input.GetKey("a"))
+        if (pawType == PawType.Left && (Input.GetKey(KeyCode.LeftArrow) || Input.GetKey("a")))
         {
             Move();
             hasMoved = true;
         }
-        } 
-
-        if (gameObject.name == "PawRight")
-        {
-            if (Input.GetKey(KeyCode.RightArrow) || Input.GetKey("d"))
+        else if (pawType == PawType.Right && (Input.GetKey(KeyCode.RightArrow) || Input.GetKey("d")))
         {
             Move();
             hasMoved = true;
         }
-        }
 
-        
-
-        // decrease timer
         if (actionTimer > 0)
         {
             actionTimer -= Time.deltaTime;
@@ -52,55 +50,34 @@ public class SmashTheNotes : MonoBehaviour
         {
             if (hasMoved && !Physics.CheckSphere(transform.position, colliderRadius, noteLayer))
             {
-                if (gameObject.name == "PawLeft")
-                {
-                    Debug.Log("Miss PawLeft");
-                }
-                else if (gameObject.name == "PawRight")
-                {
-                    Debug.Log("Miss PawRight");
-                }
+                Debug.Log($"Miss {pawType}");
+                BadLayer?.Invoke();
+                ReturnToInitialPosition();
             }
-            ReturnToInitialPosition();
-            hasMoved = false;
         }
 
         if (!hitDetected && Physics.CheckSphere(transform.position, colliderRadius, noteLayer))
         {
             hitDetected = true;
-            if (gameObject.name == "PawLeft")
-            {
-                Debug.Log("Hit PawLeft");
-            }
-            else if (gameObject.name == "PawRight")
-            {
-                Debug.Log("Hit PawRight");
-            }
+            Debug.Log($"Hit {pawType}");
+            GoodLayer?.Invoke();
             ReturnToInitialPosition();
-            hasMoved = false;
         }
     }
-    
+
     private void Move()
     {
-        var newPosition = new Vector3(this.transform.position.x, this.transform.position.y, -1.0f);
-        
-        if (gameObject.name == "PawLeft")
-        {
-            newPosition = new Vector3(this.transform.position.x, _offsetLeft, -1.0f);
-        } 
-        else 
-        {
-            newPosition = new Vector3(this.transform.position.x, _offsetRight, -1.0f);
-        }
+        var offset = pawType == PawType.Left ? _offsetLeft : _offsetRight;
+        var newPosition = new Vector3(this.transform.position.x, offset, -1.0f);
         this.GetComponent<Rigidbody>().MovePosition(newPosition);
         actionTimer = 0.1f; // reset timer
     }
-    
+
     private void ReturnToInitialPosition()
     {
         this.transform.position = initialPosition;
         actionTimer = 0.1f; // reset timer
         hitDetected = false; // reset hit detection
+        hasMoved = false;
     }
 }
